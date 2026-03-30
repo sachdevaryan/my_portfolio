e697c905a1813268d6fbe0df9dcbd6938defe9e6
Aryan <aryan01001s@gmail.com>
2026-03-31 01:56:22 +0530

    hero

---
+++ src/components/HeroText.jsx
@@ -0,0 +1,31 @@
+import { motion } from "motion/react";
+import {FlipWords} from "./FlipWords";
+
+const HeroText = () => {
+    return (
+        <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
+            {/* Desktop */}
+            <div className="flex-col hidden md:flex c-space">
+                <motion.h1 className="text-4xl font-medium" initial={{ opacity: 0 ,x:-50}} animate={{ opacity: 1 , x:0}} transition={{delay:1}}>Hi I'm Aryan</motion.h1>
+                <div className="flex flex-col items-start">
+                    <motion.p className="text-5xl font-medium text-neutral-300" initial={{ opacity: 0 ,x:-50}} animate={{ opacity: 1 , x:0}} transition={{delay:1.2}}>A Full Stack Developer</motion.p>
+                    <motion.div initial={{ opacity: 0 ,x:-50}} animate={{ opacity: 1 , x:0}} transition={{delay:1.5}}>
+                        <FlipWords words={["Secure","Modern","Scalable"]} className="font-black text-white text-8xl"/>
+                    </motion.div>
+                </div>
+                <motion.p className="text-4xl font-medium text-neutral-300" initial={{ opacity: 0 ,x:-50}} animate={{ opacity: 1 , x:0}} transition={{delay:1.8}}>AIML Enthusiast</motion.p>
+            </div>
+            {/* Mobile */}
+            <div className="flex-col flex space-y-6 md:hidden">
+                <motion.p className="text-4xl font-medium " initial={{ opacity: 0 ,x:-50}} animate={{ opacity: 1 , x:0}} transition={{delay:1}}>Hi I'm Aryan</motion.p>
+                <div>
+                    <motion.p className="text-5xl font-black text-neutral-300" initial={{ opacity: 0 ,x:-50}} animate={{ opacity: 1 , x:0}} transition={{delay:1.2}}>Building</motion.p>
+                    <motion.div initial={{ opacity: 0 ,x:-50}} animate={{ opacity: 1 , x:0}} transition={{delay:1.5}}><FlipWords words={["Secure","Modern","Scalable"]} className="font-bold text-white text-7xl"/></motion.div>
+                    <motion.p className="text-4xl font-black text-neutral-300" initial={{ opacity: 0 ,x:-50}} animate={{ opacity: 1 , x:0}} transition={{delay:1.8}}>Web Applications</motion.p>
+                </div> 
+            </div>
+        </div>
+    );
+};
+
+export default HeroText;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import HeroText from "../components/HeroText";
 
 const Hero = () => {
     return (
-        <div>
-            Hero
-        </div>
+        <section className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
+            <HeroText />
+        </section>
     );
 };
 
